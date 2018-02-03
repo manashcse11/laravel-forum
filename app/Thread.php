e9a5eacb048a2b6b57bf12ca09f624134a0d468f
Manash Chakrobortty <manash.pstu@gmail.com>
2018-02-03 13:28:15 +0600

    Test: Authenticated user may reply to thread

--- app/Thread.php
+++ app/Thread.php
@@ -6,11 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
+    protected $guarded = [];
+    
     public function path(){
         return '/threads/' . $this->id;
     }
     
     public function replies(){
         return $this->hasMany(Reply::class);
+    }
+
+    public function creator(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function addReply($reply){
+        return $this->replies()->create($reply);
     }
 }

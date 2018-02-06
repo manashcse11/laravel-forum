69c02b570391bd15fb622f98e9b921eef61b9834
Manash Chakrobortty <manash.pstu@gmail.com>
2018-02-06 21:42:56 +0600

    Test: Make some helper for test

--- tests/Feature/CreateThreadTest.php
+++ tests/Feature/CreateThreadTest.php
@@ -13,14 +13,15 @@ class CreateThreadTest extends TestCase
 
     public function test_guest_may_not_create_thread()
     {
-        $thread = factory('App\Thread')->make();
+        $thread = make('App\Thread'); //factory('App\Thread')->make();
         $this->post('/threads', $thread->toArray())
             ->assertRedirect('/login');
     }
     public function test_an_authenticated_user_can_create_thread()
     {
-        $this->be(factory('App\User')->create());
-        $thread = factory('App\Thread')->make();
+        // $this->be(create('App\User'));
+        $this->signIn();
+        $thread = make('App\Thread');
         $this->post('/threads', $thread->toArray());
         $this->get($thread->path())
             ->assertSee($thread->title)

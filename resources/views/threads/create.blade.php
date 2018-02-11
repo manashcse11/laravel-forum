1869ec88783ebb957dd5ecf27cc077091bd1d22f
Manash Chakrobortty <manash.pstu@gmail.com>
2018-02-11 13:59:40 +0600

    Add View Composer for channels

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -14,7 +14,7 @@
                             <label for="channel_id">Choose a Channel</label>
                             <select class="form-control" name="channel_id" id="channel_id">
                                 <option value="">Choose one...</option>
-                                @foreach(App\Channel::all() as $channel)
+                                @foreach($channels as $channel)
                                     <option value="{{$channel->id}}">{{$channel->name}}</option>
                                 @endforeach
                             </select>
